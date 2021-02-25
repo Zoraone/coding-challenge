e11289d68f04f73f6866199109a30b89a4bb04e6
Peng <zoracle123@hotmail.com>
2021-02-26 09:08:14 +1300

    Implement working captial ratio

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
   revenue := 0
   expenses := 0
   grossProfit := 0
+  assets := 0
+  liabilities := 0
   for _, ad := range accountData {
     totalAsInt := int(ad.TotalValue * 100)
 
@@ -24,6 +26,32 @@ func main() {
       revenue += totalAsInt
     case "expense":
       expenses += totalAsInt
+    case "assets":
+      switch ad.ValueType {
+      case "debit":
+        switch ad.AccountType {
+        case "current", "bank", "current_accounts_receivable":
+          assets += totalAsInt
+        }
+      case "credit":
+        switch ad.AccountType {
+        case "current", "bank", "current_accounts_receivable":
+          assets -= totalAsInt
+        }
+      }
+    case "liability":
+      switch ad.ValueType {
+      case "debit":
+        switch ad.AccountType {
+        case "current", "current_accounts_payable":
+          liabilities += totalAsInt
+        }
+      case "credit":
+        switch ad.AccountType {
+        case "current", "current_accounts_payable":
+          liabilities -= totalAsInt
+        }
+      }
     }
 
     if ad.AccountType == "sales" && ad.ValueType == "debit" {
@@ -37,6 +65,7 @@ func main() {
   fmt.Println(expenses)
   fmt.Println(float64(grossProfit) / float64(revenue))
   fmt.Println(float64(netProfit) / float64(revenue))
+  fmt.Printf("%.1f%%", (float32(assets) / float32(liabilities) * 100))
 }
 
 func readFile() ([]AccountData, error) {
